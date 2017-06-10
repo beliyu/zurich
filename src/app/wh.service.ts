e7e663f8d0fcd2a4fef7bb37ffabb3a443e8dc3e
Dragan Savic <drsavicrs@gmail.com>
2017-06-10 19:02:57 +0200

    ServHandleError

--- src/app/wh.service.ts
+++ src/app/wh.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class WhModel {
@@ -18,22 +22,31 @@ export class WhmService {
     constructor(private _http: Http) {
         this.headers = new Headers({'Content-Type': 'application/json'});
     };
-    getAll() {
-        return this._http.get(this._url).map((res: Response) => res.json());
+    public getAll(): Observable<WhModel[]> {
+        return this._http.get(this._url).map((res: Response) => res.json())
+        .catch(this.handleError);
     };
-    getOne(i) {
-        return this._http.get(this._url + '/' + i).map(res => res.json());
+    public getOne(i: string): Observable<WhModel> {
+        return this._http.get(this._url + '/' + i).map(res => res.json())
+        .catch(this.handleError);
     };
-    del(i) {
-        return this._http.delete(this._url + '/' + i).map(res => res.json());
+    public del(i: string): Observable<null> {
+        return this._http.delete(this._url + '/' + i).map(res => res.json())
+        .catch(this.handleError);
     };
-    add(w) {
+    public add(w: WhModel): Observable<any> {
         return this._http.post(this._url, JSON.stringify(w), {headers: this.headers})
-        .map(res => res.json());
+        .map(res => res.json())
+        .catch(this.handleError);
     };
-    togg(w) {
+    public togg(w: WhModel): Observable<any> {
         let nb = (w.boja === 'bl') ?  'rd'  :  'bl';
         return this._http.patch(this._url + '/' + w.id, JSON.stringify({'boja' : nb}),
-          {headers: this.headers}).map(res => res.json());
+          {headers: this.headers}).map(res => res.json())
+        .catch(this.handleError);
     };
+    private handleError (error: Response | any) {
+    console.error('ApiService::handleError', error);
+    return Observable.throw(error);
+  }
 }
